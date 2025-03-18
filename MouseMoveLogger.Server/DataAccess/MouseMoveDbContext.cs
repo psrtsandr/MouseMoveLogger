9f2c38af6fd7cad3761ca18b81cca45b7d1340f0
Andrey Pisarets <pisarets.andrey@gmail.com>
2025-03-18 15:13:54 +0300

    Added DbContext

---
+++ MouseMoveLogger.Server/DataAccess/MouseMoveDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MouseMoveLogger.Server.Models;
+
+namespace MouseMoveLogger.Server.DataAccess;
+
+public class MouseMoveDbContext(DbContextOptions<MouseMoveDbContext> options) : DbContext(options)
+{
+    public DbSet<MouseMoveEntry> MouseMoveEntries => Set<MouseMoveEntry>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<MouseMoveEntry>(b =>
+        {
+            b.ToTable("MouseMoveEntries");
+            b.HasKey(e => e.Id);
+            b.OwnsMany(e => e.Events, ev => ev.ToJson());
+        });
+    }
+}
