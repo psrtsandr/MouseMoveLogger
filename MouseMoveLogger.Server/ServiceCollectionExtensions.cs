5fb7bd1659a51b413842bf0cc9c166093607c6ef
Andrey Pisarets <pisarets.andrey@gmail.com>
2025-03-18 15:15:21 +0300

    Added extension to register DbContext

---
+++ MouseMoveLogger.Server/ServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MouseMoveLogger.Server.DataAccess;
+
+namespace MouseMoveLogger.Server;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMouseMoveDbContext(this IServiceCollection services) => services.AddDbContext<MouseMoveDbContext>(options => options.UseSqlite(connectionString: "Data Source=MouseMoveLogger.db;"));
+}
